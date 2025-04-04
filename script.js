3cf0e85b8990b37a9ab0ab94333c5bbe5bdf1e92
Amir <amir800900@mail.ru>
2025-04-04 19:26:01 +0300

    hw1

---
+++ script.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const hamburger = document.getElementById('hamburger');
+  const navLinks = document.getElementById('nav-links');
+  
+  hamburger.addEventListener('click', function() {
+    navLinks.classList.toggle('active');
+  });
+  
+  document.addEventListener('click', function(event) {
+    if (!event.target.closest('.nav')) {
+      navLinks.classList.remove('active');
+    }
+  });
+});
